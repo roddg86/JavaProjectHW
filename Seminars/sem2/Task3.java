134a7547dddb8e2ae2d26341e9da24fd1214add3
roddg <roddg@mail.ru>
2023-02-14 21:37:54 +0900

    task 3 add

--- Seminars/sem2/Task3.java
+++ Seminars/sem2/Task3.java
@@ -1,5 +1,7 @@
 package Seminars.sem2;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Task3 {
@@ -8,16 +10,32 @@ public class Task3 {
         Integer n = 10;
         String text = "TEST";
         String file_name = "Seminars/sem2/1.txt";
+        File file = new File(file_name);
         try {
-            FileWriter writer = new FileWriter(file_name, true);
+            FileWriter writer = new FileWriter(file, true);
             for (int i = 0; i < n; i++) {
                 writer.write(text);
+                writer.write("\n");
             }
             writer.close();
             System.out.println("Получилось");
         } catch (Exception e) {
             System.out.println("Что то пошло не так");
         }
+
+        // прочитать файл
+        try {
+            FileReader f = new FileReader(file);
+            char[] a = new char[(int) file.length()];
+            f.read(a);
+            for (char c : a) {
+                System.out.print(c);
+            }
+            f.close();
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так");
+        }
+
     }
 
 }
