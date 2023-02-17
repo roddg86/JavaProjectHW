7d5e6a8dd6378d68f271a41b3c93ff67d7759bd7
Dmitry <110647769+roddg86@users.noreply.github.com>
2023-02-17 14:45:21 +0900

    Task1 solved

--- Seminars/sem3/Task1.java
+++ Seminars/sem3/Task1.java
@@ -1,9 +1,29 @@
 package Seminars.sem3;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Task1 {
     public static void main(String[] args) {
         // Заполнить список десятью случайными числами. Отсортировать список методом sort() и вывести его на экран.
-        
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list = genRandomArray(10,100); 
+        System.out.println(list);
+
     }
     
+    public static ArrayList<Integer> genRandomArray(int size, int maxValue) {
+        /* метод генерирует список случайных чисел */
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Random rand = new Random();
+
+        for (int i = 0; i < size; i++) {
+            list.add(i, rand.nextInt(maxValue));
+            list.sort(null);
+        }
+        return list;
+    }
 }
