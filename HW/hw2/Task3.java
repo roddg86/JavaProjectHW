83e3c036b55d0aee2fdd3204c719810b9d1086ae
Dmitry <110647769+roddg86@users.noreply.github.com>
2023-02-17 09:57:36 +0900

    task 3 solved

---
+++ HW/hw2/Task3.java
@@ -0,0 +1,23 @@
+package HW.hw2;
+
+public class Task3 {
+    public static void main(String[] args) {
+        // Напишите метод, который принимает на вход строку (String) и определяет
+        // является ли строка палиндромом (возвращает boolean значение).
+        String text = "tttrrruuuuuurrrttt    ,";
+        Boolean result = isPalindrome(text);
+        System.out.println(result);
+
+    }
+
+    public static boolean isPalindrome(String text) {
+        text = text.replaceAll("\\W", "");// удаляем все ненужное
+        StringBuilder sb = new StringBuilder(text);
+        sb.reverse(); // переворачиваем строку
+        String invertedText = sb.toString();// присваиваем перевернутую строку
+
+        return text.equalsIgnoreCase(invertedText);// возвращаем сравнение двух строк вне зависимости от регистра
+
+    }
+
+}
