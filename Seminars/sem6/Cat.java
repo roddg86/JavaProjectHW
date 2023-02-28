ccbfa95f0515eb8e4d9981bd71fa4299d63c0936
Dmitry <110647769+roddg86@users.noreply.github.com>
2023-02-28 17:50:05 +0900

    add user class

---
+++ Seminars/sem6/Cat.java
@@ -0,0 +1,51 @@
+package Seminars.sem6;
+
+public class Cat {
+    private String name_cat;
+    private int age;
+    private String breed;
+    private String color;
+    private String gender;
+    private String name_owner;
+    private int id;
+    private boolean vaccinated;
+
+    public void setName_cat(String name_cat) {
+        this.name_cat = name_cat;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setName_owner(String name_owner) {
+        this.name_owner = name_owner;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d %s %s %s %d", name_cat, name_owner, age, breed, color, gender, id);
+    }
+
+
+    
+}
